d4322ccee7ee91c9f95270e18c4adee956e23661
2121050486 <2121050486@student.humg.edu.vn>
2025-01-02 23:17:20 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace hoten_1234_2425_BAITHITEST_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
